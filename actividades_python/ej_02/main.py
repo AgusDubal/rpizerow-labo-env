da9bd488190e0991743f73fff65dc4cfe0c2f9b2
AgusDubal <agustin.dubal06@gmail.com>
2024-04-10 21:15:43 +0100

    feat: programa que prende los diferentes colores secuencialmente de un LED RGB

--- actividades_python/ej_02/main.py
+++ actividades_python/ej_02/main.py
@@ -1,21 +1,25 @@
+#Importo las funciones LED y sleep de las librerías gpiozero y time respectivamente
 from gpiozero import LED
 from time import sleep
 
+#Defino los pines asignados para cada color del LED
 ledverde = LED(13)
 ledrojo = LED(19)
 ledazul = LED(26)
 
 while True:
+
+	#Prendo el LED verde y 250ms despues apago el LED rojo
 	ledverde.on()
 	sleep(0.25)
 	ledrojo.off()
 
-
+	#Prendo el LED azul y  500ms despues apago el LED verde
 	ledazul.on()
 	sleep(0.5)
 	ledverde.off()
 
-
+	#Prendo el LED rojo y 1s despues apago el LED azul
 	ledrojo.on()
 	sleep(1)
 	ledazul.off()
